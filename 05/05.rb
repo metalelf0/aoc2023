3f8896f6ee1b150f486976a07b40407c5040b5fc
Andrea Schiavini <metalelf0@gmail.com>
2023-12-05 23:21:47 +0100

    feat: will basic arithmetic be enough for part 2?

--- 05/05.rb
+++ 05/05.rb
@@ -16,11 +16,43 @@ class Array
     end
     result << arr
   end
+
+  def in_groups_of(number, fill_with = nil)
+    if number.to_i <= 0
+      raise ArgumentError,
+        "Group size must be a positive integer, was #{number.inspect}"
+    end
+
+    if fill_with == false
+      collection = self
+    else
+      # size % number gives how many extra we have;
+      # subtracting from number gives how many to add;
+      # modulo number ensures we don't add group of just fill.
+      padding = (number - size % number) % number
+      collection = dup.concat(Array.new(padding, fill_with))
+    end
+
+    if block_given?
+      collection.each_slice(number) { |slice| yield(slice) }
+    else
+      collection.each_slice(number).to_a
+    end
+  end
 end
 
 def seeds_from_file(source_file:)
   input = File.readlines(source_file).map(&:strip)
   input.split('').first.first.split(":").last.split(" ").map(&:to_i)
+end
+
+def seeds_from_file_part2(source_file:)
+  seeds = []
+  input = File.readlines(source_file).map(&:strip)
+  input.split('').first.first.split(":").last.split(" ").map(&:to_i).in_groups_of(2).each do |start, range_length|
+    seeds << { start: start, range_length: range_length }
+  end
+  seeds
 end
 
 # input: an array of lines defining ranges
@@ -59,18 +91,25 @@ def find_target(number:, conversion_ranges: {})
 end
 
 source_file = "input.txt"
-seeds = seeds_from_file(source_file:)
+# seeds = seeds_from_file(source_file:)
+
+seeds = seeds_from_file_part2(source_file:)
+p seeds.size
 
 conversions = conversions_from_file(source_file:)
 
-conversions.each do |conversion_ranges|
-  new_numbers = seeds.map do |number|
-    find_target(number:, conversion_ranges:)
+lowest = 1/0.0 # Infinity
+
+seeds.each do |seed_range|
+  seed_range[:start].upto(seed_range[:start] + seed_range[:range_length]) do |number|
+    conversions.each do |conversion_ranges|
+      number = find_target(number:, conversion_ranges:)
+    end
+    if number < lowest
+      lowest = number
+      puts "New lowest is #{lowest}"
+    end
   end
-  seeds = new_numbers
-  print "."
 end
 
-p seeds
-
-p seeds.min
+puts lowest
